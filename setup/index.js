21006aa3fe1f6436b5c87d0e776dc44e3da8067f
AnishMandal939 <mandalktaanees12@gmail.com>
2024-05-30 08:53:27 +0545

    fix - changing quantity to payload acc to redux pattern

--- setup/index.js
+++ setup/index.js
@@ -10,14 +10,14 @@ const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 function orderCake(){
   return {
     type: CAKE_ORDERED,
-    quantity: 1
+    payload: 1
   }
 }
 
 function reStockCake(qty=1){
   return{
     type: CAKE_RESTOCKED,
-    quantity: qty
+    payload: qty
   }
 }
 //(previousState, action) => newState
@@ -37,7 +37,7 @@ const reducer = (state = initialState, action) => {
       case CAKE_RESTOCKED:
         return{
           ...state,
-          numOfCakes: state.numOfCakes + action.quantity
+          numOfCakes: state.numOfCakes + action.payload
       }
       default:
         return state;
